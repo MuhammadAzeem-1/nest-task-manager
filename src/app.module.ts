73a3f6c243510c91deabf1c47580fbc9eaeba920
Muhammad Azeem <mazeem182000@gmail.com>
2025-05-22 15:03:53 +0500

    feat: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './modules/tasks/tasks.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [TasksModule, PrismaModule],
+  imports: [TasksModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
