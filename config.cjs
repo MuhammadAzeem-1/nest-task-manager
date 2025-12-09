6a10b38a37e9e24b2f48755a5eaff2f011e83cfb
Muhammad Azeem <mazeem182000@gmail.com>
2025-12-10 01:09:13 +0500

    fix: updaye config file

--- config.cjs
+++ config.cjs
@@ -1,15 +1,13 @@
 module.exports = {
-    apps: [
-      {
-        name: "nest-app",
-        script: "dist/main.js",
-        cwd: "/home/ec2-user/nest-task-manager",
-        instances: 1,
-        exec_mode: "fork",
-        env: {
-          NODE_ENV: "production",
-        },
-      }
-    ]
-  }
-  
+  apps: [
+    {
+      name: "nest-app",
+      script: "dist/main.js",
+      env: {
+        NODE_ENV: "production",
+        DATABASE_URL: process.env.DATABASE_URL,  // required
+        PORT: process.env.PORT || 3000,
+      },
+    },
+  ],
+};
