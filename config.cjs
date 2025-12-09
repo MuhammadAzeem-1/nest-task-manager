7eec21d441c70935b7f4ef2d6ee977cdfde86413
Muhammad Azeem <mazeem182000@gmail.com>
2025-12-10 00:51:44 +0500

    fix: update config file and tsconfig

--- config.cjs
+++ config.cjs
@@ -3,15 +3,12 @@ module.exports = {
       {
         name: "nest-app",
         script: "dist/main.js",
-        cwd: "/home/ec2-user/backend",
+        cwd: "/home/ec2-user/nest-task-manager",
         instances: 1,
         exec_mode: "fork",
         env: {
           NODE_ENV: "production",
         },
-        env_production: {
-          NODE_ENV: "production"
-        }
       }
     ]
   }
