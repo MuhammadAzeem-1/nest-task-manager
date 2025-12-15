da5371406df073a30ed6e160379d1674d246e9cb
Muhammad Azeem <mazeem182000@gmail.com>
2025-12-15 23:34:17 +0500

    fix: update yml file

--- config.cjs
+++ config.cjs
@@ -5,7 +5,6 @@ module.exports = {
       script: "dist/main.js",
       env: {
         NODE_ENV: "production",
-        DATABASE_URL: process.env.DATABASE_URL,  // required
         PORT: process.env.PORT || 3000,
       },
     },
